755ad354546c47d7859aeb6477ab5c043f805abf
Santosh Acharya <santosh.acharya1200@gmail.com>
2021-03-30 13:45:25 +0545

    codes added

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-#first lines
+from bs4 import BeautifulSoup as bs
+import requests
+
+url="https://www.jobsnepal.com/search?q=python"
+req= requests.get(url)
+soup= bs(req.text,"html.parser")
+tags= soup.find_all('strong', abc="title tw-line-height-12")
+print(tags)
